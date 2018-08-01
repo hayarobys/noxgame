44684c62b53aa3996fbc1172a1d9dab6a158a9d2
suph <suph@dkitec.com>
2018-08-01 18:44:26 +0900

    FileVO 추가
    - 이전 커밋에서 누락된 변경 내역입니다.

--- src/main/java/kr/pe/hayarobys/nox/common/upload/FileVO.java
+++ src/main/java/kr/pe/hayarobys/nox/common/upload/FileVO.java
@@ -1,5 +1,10 @@
 package kr.pe.hayarobys.nox.common.upload;
 
 public class FileVO{
-
+	private int fileGrpFk;
+	private int memFk;
+	private int fileSize;
+	private String fileExtension;
+	private String fileOriginalNm;
+	private String fileSaveNm;
 }
